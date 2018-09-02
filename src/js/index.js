bb4cb7f671a44ec279e17425ac07ae278a448fda
digi0ps <sriru1998@gmail.com>
2018-09-02 23:08:23 +0530

    Add parallax backgrounds with dummy images.

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+$(function() {
+  $.stellar({
+    horizontalScrolling: false,
+    verticalOffset: 40,
+  });
+});
+
+// TODO: Add scroll based url updation (preferably using path.js)
+// TODO: Add scroll based element highlighting
