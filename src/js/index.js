96dfc7b20125f687e9541f1c7a577d492a465643
digi0ps <sriru1998@gmail.com>
2018-09-06 23:05:13 +0530

    Add navbar and scrolling.

--- src/js/index.js
+++ src/js/index.js
@@ -5,5 +5,24 @@ $(function() {
   });
 });
 
+// Scroll to top if the user clicks the header text
+$(".header-text").on("click", function(e) {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+// Scroll to the section on navigation li click.
+$(".navigation li").on("click", function(e) {
+  const sectionName = e.target.dataset.sectionName;
+  const section = $(`.section[data-section=${sectionName}]`)[0];
+  window.section = section;
+  window.scrollTo({
+    top: section.getBoundingClientRect().top,
+    behavior: "smooth",
+  });
+});
+
 // TODO: Add scroll based url updation (preferably using path.js)
 // TODO: Add scroll based element highlighting
