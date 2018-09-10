8b83fde1007e576cc92fac96917a415d325e1d7b
digi0ps <sriru1998@gmail.com>
2018-09-10 18:02:13 +0530

    Added new content and removed nav fill.

--- src/js/routing.js
+++ src/js/routing.js
@@ -3,26 +3,34 @@ All hash-based routes are defined here.
 Updates the hash of the page, based on the section using Path.js
 */
 
-Path.root("#/gallery");
-
-Path.map("#/team").to(function() {
-  utils.scrollToSection("team");
-});
-
-Path.map("#/projects").to(function() {
-  utils.scrollToSection("projects");
-});
+Path.root("");
 
 Path.map("#/intro").to(function() {
   utils.scrollToSection("intro");
 });
 
-Path.map("#/about").to(function() {
-  utils.scrollToSection("about");
+Path.map("#/adwprimary").to(function() {
+  utils.scrollToSection("adwprimary");
 });
 
-Path.map("#/functionalteams").to(function() {
-  utils.scrollToSection("functionalteams");
+Path.map("#/angilam").to(function() {
+  utils.scrollToSection("angilam");
+});
+
+Path.map("#/naik").to(function() {
+  utils.scrollToSection("naik");
+});
+
+Path.map("#/wishtree").to(function() {
+  utils.scrollToSection("wishtree");
+});
+
+Path.map("#/carnival").to(function() {
+  utils.scrollToSection("carnival");
+});
+
+Path.map("#/kumizhi").to(function() {
+  utils.scrollToSection("kumizhi");
 });
 
 Path.map("#/contacts").to(function() {
